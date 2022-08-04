b592cc44cfbe8b7b0946d98ca651745a3574ac0c
Dmitry Davidyk <91990771+DmitryDavidyuk@users.noreply.github.com>
2022-08-04 23:42:26 +0300

    contact form hook

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -1,64 +1,119 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import CSS from './Contacts.module.css';
 
-class ContactsForm extends Component {
-  state = {
-    name: '',
-    number: '',
+export default function ContactsForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  nameInputId = nanoid(5);
-  phoneInputId = nanoid(5);
-
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
+
+    onSubmit({ name, number });
+    setName('');
+    setNumber('');
   };
 
-  reset() {
-    this.setState({ name: '', number: '' });
-  }
+  const nameInputId = nanoid(5);
+  const phoneInputId = nanoid(5);
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <form className={CSS.form} onSubmit={this.handleSubmit}>
-        <label htmlFor={this.nameInputId}>Ім'я</label>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Назва може містити лише літери, апостроф, тире та пробіли. Наприклад Адріан, Джейкоб Мерсер, Шарль де Бац де Кастельмор д'Артаньян"
-          required
-          value={name}
-          onChange={this.handleChange}
-          id={this.nameInputId}
-        />
-        <label htmlFor={this.phoneInputId}>Телефон</label>
-        <input
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          value={number}
-          placeholder="+380123456789"
-          maxLength="13"
-          minLength="10"
-          onChange={this.handleChange}
-          id={this.phoneInputId}
-          title="Номер телефону повинен складатися з цифр в форматі +380123456789"
-          required
-        />
-        <button type="submit">Add contact</button>
-      </form>
-    );
-  }
+  return (
+    <form className={CSS.form} onSubmit={handleSubmit}>
+      <label htmlFor={nameInputId}>Ім'я</label>
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Назва може містити лише літери, апостроф, тире та пробіли. Наприклад Адріан, Джейкоб Мерсер, Шарль де Бац де Кастельмор д'Артаньян"
+        required
+        value={name}
+        onChange={handleChange}
+        id={nameInputId}
+      />
+      <label htmlFor={phoneInputId}>Телефон</label>
+      <input
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        value={number}
+        placeholder="+380123456789"
+        maxLength="13"
+        minLength="10"
+        onChange={handleChange}
+        id={phoneInputId}
+        title="Номер телефону повинен складатися з цифр в форматі +380123456789"
+        required
+      />
+      <button type="submit">Add contact</button>
+    </form>
+  );
 }
 
-export default ContactsForm;
+// class OldContactsForm extends {
+//   // state = {
+//   //   name: '',
+//   //   number: '',
+//   // };
+
+//   // nameInputId = nanoid(5);
+//   // phoneInputId = nanoid(5);
+
+//   // handleChange = e => {
+//   //   const { name, value } = e.currentTarget;
+//   //   this.setState({ [name]: value });
+//   // };
+
+//   // handleSubmit = e => {
+//   //   e.preventDefault();
+//   //   this.props.onSubmit(this.state);
+//   //   this.reset();
+//   // };
+
+//   render() {
+//     const { name, number } = this.state;
+//     return (
+//       <form className={CSS.form} onSubmit={this.handleSubmit}>
+//         <label htmlFor={this.nameInputId}>Ім'я</label>
+//         <input
+//           type="text"
+//           name="name"
+//           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//           title="Назва може містити лише літери, апостроф, тире та пробіли. Наприклад Адріан, Джейкоб Мерсер, Шарль де Бац де Кастельмор д'Артаньян"
+//           required
+//           value={name}
+//           onChange={this.handleChange}
+//           id={this.nameInputId}
+//         />
+//         <label htmlFor={this.phoneInputId}>Телефон</label>
+//         <input
+//           type="tel"
+//           name="number"
+//           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//           value={number}
+//           placeholder="+380123456789"
+//           maxLength="13"
+//           minLength="10"
+//           onChange={this.handleChange}
+//           id={this.phoneInputId}
+//           title="Номер телефону повинен складатися з цифр в форматі +380123456789"
+//           required
+//         />
+//         <button type="submit">Add contact</button>
+//       </form>
+//     );
+//   }
+// }
